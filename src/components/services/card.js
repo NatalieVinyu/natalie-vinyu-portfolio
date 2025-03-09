a1636997ca2b241880fee3e2ce8ede271bc115ef
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-03-09 13:51:43 +0200

    rendering lists

--- src/components/services/card.js
+++ src/components/services/card.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 // Named export of the Card component
-export const Card = ({ title, imageUrl }) => {
+export const Card = ({ title, imageUrl, list }) => {
   return (
     <div className='container mx-auto'>
       <div className='flex align-center items-center'>      
         <div className='max-w-sm rounded overflow-hidden shadow-lg bg-white m-4'>        
             <div className='px-6 py-4'>
-                <div className='font-bold text-xl mb-2'>
+                <div className='text-center font-bold text-4xl mb-2'>
                   {title}
                 </div>
-                <p className='text-gray-700 text-base'>{imageUrl}</p>
+                <img className='w-full' src={imageUrl} />
+                {list && list.length > 0 && (
+                  <ul>
+                    {list.map((item, index) => (
+                    <li key={index}>
+                      {item}
+                    </li>
+                    ))}
+                  </ul>
+                )}
             </div>
-            <div className='px-6 pt-4 pb-2'>
-                <button className='bg-blue-500 py-2 px-4 rounded hover:bg-blue-700'>Read more</button>
-            </div>
+            
         </div>
       </div>
     </div>
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -2,12 +2,30 @@ import React from 'react';
 import { Card } from './card';
 
 export const Services = () => {
+  const serviceList = [
+    "1",
+    "2",
+    "3"
+  ];
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="container flex justify-center items-center min-h-screen">
       <Card 
         title="Virtual Assistant"
         imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
+        list={['item 1', 'item 2', 'item 3']}
       /> 
+      <Card 
+        title="Virtual Assistant"
+        imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
+        list={['item 4', 'item 5', 'item 6']}
+      />
+      <Card 
+        title="Virtual Assistant"
+        imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
+        list={['item 7', 'item 8', 'item 9']}
+      />
+
     </div>
   );
 };
