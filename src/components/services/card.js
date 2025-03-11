0295ee223b05ac228c812f42923c57853cbf5c63
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-03-11 10:42:06 +0200

    added lists

--- src/components/services/card.js
+++ src/components/services/card.js
@@ -5,14 +5,14 @@ export const Card = ({ title, imageUrl, list }) => {
   return (
     <div className='container mx-auto'>
       <div className='flex align-center items-center'>      
-        <div className='max-w-sm rounded overflow-hidden shadow-lg bg-white m-4'>        
+        <div className='max-w-sm rounded overflow-hidden shadow-lg bg-white m-4 w-200 h-200'>        
             <div className='px-6 py-4'>
                 <div className='text-center font-bold text-4xl mb-2'>
                   {title}
                 </div>
                 <img className='w-full' src={imageUrl} />
                 {list && list.length > 0 && (
-                  <ul>
+                  <ul className='list-disc'>
                     {list.map((item, index) => (
                     <li key={index}>
                       {item}
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -2,30 +2,40 @@ import React from 'react';
 import { Card } from './card';
 
 export const Services = () => {
-  const serviceList = [
-    "1",
-    "2",
-    "3"
-  ];
-
   return (
-    <div className="container flex justify-center items-center min-h-screen">
+    <div className="container mx-auto  min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       <Card 
-        title="Virtual Assistant"
+        title="Email Management"
         imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
-        list={['item 1', 'item 2', 'item 3']}
+        list={['Flagging Emails', 'Clearing Spam', 'Replying to Emails']}
       /> 
       <Card 
-        title="Virtual Assistant"
+        title="General Administration"
         imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
-        list={['item 4', 'item 5', 'item 6']}
+        list={['Data Entry', 'Excel', 'Appointment Setting', 'Research', 'Typing', 'Proof Reading']}
       />
       <Card 
-        title="Virtual Assistant"
+        title="Calender"
         imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
-        list={['item 7', 'item 8', 'item 9']}
+        list={['Email Remainders', 'Create Meeting Agendas', 'Organizing Meetings', 'Booking Appointments']}
       />
+      <Card 
+        title="Travel Management"
+        imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
+        list={['Flights Bookings', 'Accommodation', 'Car Rentals & Transfers', 'Visa & Travel Insurance Assistance', 'Personalized Itineraries & Trip Planning', 'Business Trip Scheduling & Expense Tracking']}
+      /> 
+      <Card 
+        title="Event Management"
+        imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
+        list={['Corporate, Workshop', 'Conferencing', 'Meeting & Social Event Planning', 'Location Scouting', 'Budgeting', 'Point of contact']}
+      />
+      <Card 
+        title="Other"
+        imageUrl='https://aristosourcing.com/wp-content/uploads/2021/10/personal-assistant-blog.jpg'
+        list={['Online Shopping and Sourcing', 'Sending Thank You Cards', 'Paying Bills', 'Running/Managing Competitions', 'Lifestyle management', 'Website Design & Administration']}
+      />
+      </div>
 
     </div>
-  );
-};
+  )};
