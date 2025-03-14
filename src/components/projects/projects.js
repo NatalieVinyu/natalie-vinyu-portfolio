5d3f277d3bded900de354b56d01fb88bed757c71
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-03-14 16:31:10 +0200

    added image url

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,12 +1,12 @@
 //named export
 
-export const Projects = ({ title, image, link}) => {
+export const Projects = () => {
   return (
     <div className='container mx-auto bg-cyan-300'>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ">
-        <div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-6 m-6">
+        <div className="p-4">
           <h1 className="text-4xl">Oh So Heavenly</h1>
-          <img src="public/images/Screenshot 2025-03-14 at 09.22.24.png"/>
+          <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/160/244/original/Screenshot_2025-03-14_at_09.22.24.png?1741962291" />
           <div className="flex gap-20">
             <a>GitHub</a>
             <a>Project</a>
