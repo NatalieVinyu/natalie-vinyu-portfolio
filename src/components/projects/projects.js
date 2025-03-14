439dbb0f51cece6663262232430dd558c3a27883
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-03-14 16:16:38 +0200

    added image

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -5,7 +5,12 @@ export const Projects = ({ title, image, link}) => {
     <div className='container mx-auto bg-cyan-300'>
       <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ">
         <div>
-          Oh So Heavenly
+          <h1 className="text-4xl">Oh So Heavenly</h1>
+          <img src="public/images/Screenshot 2025-03-14 at 09.22.24.png"/>
+          <div className="flex gap-20">
+            <a>GitHub</a>
+            <a>Project</a>
+          </div>
         </div>
 
       </div>
